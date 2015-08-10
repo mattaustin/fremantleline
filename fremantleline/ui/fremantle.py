0901580871344989ce863a5dc6512c27d71a4b91
Matt Austin <mail@mattaustin.me.uk>
2015-08-10 23:10:27 +0800

    It's 2015.

--- fremantleline/ui/fremantle.py
+++ fremantleline/ui/fremantle.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Fremantle Line: Transperth trains live departure information
-# Copyright (c) 2009-2014 Matt Austin
+# Copyright (c) 2009-2015 Matt Austin
 #
 # Fremantle Line is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
